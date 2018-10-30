52465d8b780e53b5f1e993a74147bfe8cb22f8d1
Jonathan Chan <johnkchan252@gmail.com>
2018-10-30 01:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var newID = oldID + 1000000000;
 
 var ageIsValid = Number.isInteger(currentAge);
 
-var randomNumber = Math.random() * 1000000000;
+var randomNumber = Math.random() * 20;
 
 var randomInteger = Math.floor(randomNumber) + 1;
 
